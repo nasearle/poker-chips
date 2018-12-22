e5cdb2c049fe0e8fdbf548b556ea9ff76dcf9101
Nicolas Searle <nasearle@ucla.edu>
2018-12-22 15:36:49 -0800

    add index.html, table.html, new pkgs

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
-var uniqid = require('uniqid');
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const path = require('path');
+const shortid = require('shortid');
+const admin = require('firebase-admin');
+const bodyParser = require('body-parser');
 
-app.use(express.static(__dirname));
+const serviceAccount = require(`${__dirname}/poker-chipz-37b2839404fd.json`);
 
-const server = app.listen(5002, () => {
-  const host = server.address().address;
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.firestore();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/', express.static(path.join(__dirname + '/app/')));
+
+app.post('/create-table', (req, res) => {
+  const id = shortid.generate();
+  console.log(id);
+  const name = req.body.name;
+  console.log(name);
+})
+
+const server = app.listen(8080, () => {
+  // const host = server.address().address;
   const port = server.address().port;
-  console.log('App listening at http://%s:%s', host, port);
+  console.log(`server up on localhost:${port}`);
 });
